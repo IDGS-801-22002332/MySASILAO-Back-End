dc23503f68ee525aef244ae11485ab06db98c53f
IDGS-801-22002332 <jesusisaguirr@gmail.com>
2026-04-09 23:50:57 -0600

    Pruebas de servidor

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   // ESTA ES LA LÍNEA MÁGICA QUE QUITA EL ERROR
   app.enableCors(); 
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
